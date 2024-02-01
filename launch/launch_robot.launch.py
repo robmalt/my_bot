59b2bb7c43178239cccd57f451519ed45fefc343
Rob Malt <robmalt1@gmail.com>
2024-02-01 15:37:05 -0600

    bug fixes

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -12,8 +12,6 @@ from launch.event_handlers import OnProcessStart
 
 from launch_ros.actions import Node
 
-
-
 def generate_launch_description():
 
 
@@ -46,22 +44,21 @@ def generate_launch_description():
     
 
 
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    #robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
-                    controller_params_file]
+        parameters=[controller_params_file]
     )
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
@@ -74,7 +71,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
