04f53e6a381aceaf8f150fb6b019dcfbcd0c9d70
Rob Malt <robmalt1@gmail.com>
2024-02-12 16:43:11 -0600

    sabertooth working with ros2 control

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -46,14 +46,15 @@ def generate_launch_description():
     
 
 
-    # robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
 
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[controller_params_file]
+        parameters=[{'robot_description': robot_description},
+                    controller_params_file]
     )
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
